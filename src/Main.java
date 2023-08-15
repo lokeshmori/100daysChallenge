f3ec6013feb5ba3ae90b3503754bd16b8a4b0584
lokesh <lokeshmori4@gmail.com>
2023-08-15 18:14:54 +0530

    initial commit for this project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+          // linear search in arrays
+             // time complexity O(n)
+
+    public static int linearSearch(String arr[] , String key ){
+
+        for(int i=0; i<arr.length;i++) {
+            if (key.equals(arr[i])) {
+                return i;
+            }
+        }
+        return -1 ;
+
     }
-}
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the length of Array");
+        int  n = sc.nextInt() ;
+
+        String arr[] = new String[n];
+
+        for(int i = 0; i < n ; i++) {
+
+            System.out.println("Enter the "+i+" element of array");
+
+            arr[i] = sc.next() ;
+
+        }
+        System.out.println(" Enter the element Do you want to search");
+
+        String s = sc.next() ;
+
+         int  posi =  linearSearch(arr,s);
+
+        if( posi ==-1 ){
+            System.out.println("not found ");
+        }else{
+            System.out.println("element is at "+ posi);
+        }
+
+    }
+    }
